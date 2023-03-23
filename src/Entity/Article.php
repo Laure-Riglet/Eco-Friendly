6c815b215a8f37f2fc0c5b83c03f51c3654c4e2f
Laure-Riglet <laure.riglet@gmail.com>
2023-03-23 22:37:06 +0100

    fix: merge conflicts

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,11 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
-<<<<<<< HEAD
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-=======
->>>>>>> develop
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
